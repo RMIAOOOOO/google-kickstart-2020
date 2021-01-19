7f4de50e9f10521decbf9b9bf67ce6009d62362a
maurice <mauricehuang9@gmail.com>
2021-01-20 00:29:58 +0800

    Add note and refactor round A problem 4
    used std header, organize code from object oriented implementation to
    procedual implementation for faster implementation at competition time,
    taking reference from tmwilliamlin168 to store tree in 2d array instead
    of tree nodes, add notes with example for easier understanding.

--- round-a-2020/round-a-4-undling.cpp
+++ round-a-2020/round-a-4-undling.cpp
@@ -1,117 +1,107 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-  char character;
-  int occurrenceCount;
-  int depth;
-  vector<Node*> childNode;
-  Node* parentNode;
+const int MAX_LEN_OF_STRING = 2e6;
+const int ROOT_INDEX_ZERO = 0;
 
-public:
-  Node (char c, Node* parent, int d) {
-    character = c;
-    parentNode = parent;
-    depth = d;
-    childNode.resize(26, nullptr);
-    occurrenceCount = 0 ;
-  }
+vector<vector<int> > children_index_lookup;
+vector<int> occurrence_of_segment;
 
-  void rootInsertWord(const string &word){
-    int firstCharIndex = word[0] - 'A';
-    if(childNode[firstCharIndex] == nullptr){
-      childNode[firstCharIndex] = new Node(word[0], this, 1);
-    }
-    childNode[firstCharIndex]->nodeInsertWord(word, 0);
-  }
 
-  void nodeInsertWord(const string &word, int index){
-//    cout << "nodeInsertWord index " << index << "\n";
-    occurrenceCount ++;
-    if(index + 1 < word.length()){
-      int charIndex = word[index + 1] - 'A';
-      if(childNode[charIndex] == nullptr){
-        childNode[charIndex] = new Node(word[index + 1], this, depth + 1);
-      }
-      childNode[charIndex]->nodeInsertWord(word, index + 1);
-    }
-//    cout << "nodeInsertWord occurrence " << occurrenceCount << "\n";
-  }
-
-  int rootGetResult(int K){
-    int result = 0;
-    for(int i = 0; i < 26; ++i){
-      if(childNode[i] != nullptr) result += childNode[i]->nodeGetResult(K);
-    }
-    return result;
-  }
-
-  int nodeGetResult(int K){
-    if(occurrenceCount < K){
-      return 0;
-    }else{
-      int childrenResultsSum = 0;
-      for(int i = 0; i < 26; ++i){
-        if(childNode[i] != nullptr) childrenResultsSum += childNode[i]->nodeGetResult(K);
-      }
-      int currentNodeEffectiveOccurrence =  ( occurrenceCount / K ) * K;
-      int currentNodeScore = ( currentNodeEffectiveOccurrence / K ) * depth ;
-      parentNode->minusOccurrence(currentNodeEffectiveOccurrence);
-      int score = currentNodeScore + childrenResultsSum;
-//      cout << "nodeGetResult occurrenceCount " << occurrenceCount  << "\n";
-//      cout << "nodeGetResult character " << character << "\n";
-//      cout << "nodeGetResult childrenResultsSum" << childrenResultsSum << "\n";
-//      cout << "nodeGetResult currentNodeScore" << currentNodeScore << "\n";
-//      cout << "nodeGetResult score " << score << "\n";
-      return score;
+// 3. DFS through the tree, try to translate the occurrence count to answer points,
+//      the occurrences not used up is collected up to the parent segment.
+int dfs (int bundle_size, int parent_index = ROOT_INDEX_ZERO,
+         int current_segment_index = ROOT_INDEX_ZERO, int current_depth = 0) {
+//  3.1 Step through each possible character following the current segment, probe into it if it has an index not root
+  int children_score = 0;
+  for (int current_char_index = 0; current_char_index < 26; ++current_char_index) {
+    int new_segment_index = children_index_lookup[current_segment_index][current_char_index];
+    if (new_segment_index != ROOT_INDEX_ZERO) {
+//    3.1.1 DFS into the child and collect their completed scores.
+      children_score +=
+        dfs(bundle_size, current_segment_index, new_segment_index, current_depth + 1);
     }
   }
 
-  void minusOccurrence(int toMinus){
-    occurrenceCount -= toMinus;
-    if(parentNode != nullptr){
-      parentNode->minusOccurrence(toMinus);
-    }
-  }
-};
+//  3.2 Collect the total complete score at this segment + its children
+  int current_segment_count = occurrence_of_segment[current_segment_index] ;
+  int current_num_bundle = (current_segment_count / bundle_size);
+  int current_segment_score = current_num_bundle * current_depth;
+  int total_score = children_score + current_segment_score;
+
+// 3.3 Add back the not used up scores to its parent index count, so they can be bundled at a
+//       lower depth.
+  int remaining_segment_count = current_segment_count - current_num_bundle * bundle_size;
+  if (current_depth >= 2) occurrence_of_segment[parent_index] += remaining_segment_count;
+
+  return total_score;
+
+}
 
 void solve(){
 
   // 1. Get input
-  int N, K;
-  cin >> N >> K;
-  vector<string> inputStrings(N);
-
-  for(int i = 0; i < N; ++i){
+  int number_words, bundle_size;
+  cin >> number_words >> bundle_size;
+  vector<string> inputStrings(number_words);
+  for (int i = 0; i < number_words; ++i) {
     string in;
     cin >> in;
     inputStrings[i] = in;
   }
-  // 2. Create tree data structure
-  Node* tree = new Node(' ', nullptr, 0);
 
-  // 3. Create mailing tree
-  for(int i = 0; i < N; ++i){
-      tree->rootInsertWord(inputStrings[i]);
+
+  // 2. Translates words into trees
+  //   2.1 Children_index_lookup[i] is an array of 26 index pointed to from segment made up of
+  //         i-th segment following the next char ('A' to 'Z')
+  //       Children_index_lookup[0] is an array of 26 index pointed to from root segment,
+  //         which represents the segment "" followed by 'A' to 'Z', which are segment "A" to "Z".
+  //       A new segment is added to index if it has not been previously created.
+  //       Children_index_lookup[7][2] stores the index of the 7-th indexed segment
+  //         followed by character 'C' (2 = 'C' - 'A').
+  //       Children_index_lookup[0][11] stores the index pointed to from segment "" following 'L'
+  //         which is the index of the segment ("" + "L") = "L".
+  children_index_lookup.resize(0);
+  children_index_lookup.resize(MAX_LEN_OF_STRING + 1, vector<int>(26, 0));
+  //   2.2 occurrence_of_segment stores the occurrence of a string indexed by i
+  occurrence_of_segment.resize(0);
+  occurrence_of_segment.resize(MAX_LEN_OF_STRING  + 1, ROOT_INDEX_ZERO);
+  //   2.3 Iterate through words to construct tree and add occurrence_of_segment count
+  int next_new_index = 1;
+  for (int i = 0; i < number_words; ++i) {
+    string current_string = inputStrings[i];
+    int previous_segment_index = ROOT_INDEX_ZERO;
+    //   2.3.1 Step through a word from beginning to end
+    for (int j = 0; j < current_string.size(); ++j){
+      int current_char_index = current_string[j] - 'A';
+      // 2.3.2 If the segment following this char is not yet indexed, give it a new index
+      if (children_index_lookup[previous_segment_index][current_char_index] == ROOT_INDEX_ZERO) {
+        children_index_lookup[previous_segment_index][current_char_index] = next_new_index;
+        next_new_index ++;
+      }
+      // 2.3.3 Step into the new segment to get ready to step into next char's index
+      previous_segment_index = children_index_lookup[previous_segment_index][current_char_index];
     }
+    // 2.3.4 increment the occurrence at the last character's index
+    occurrence_of_segment[previous_segment_index] ++;
+  }
 
-  // 4. Tree traversal (DFS) to get result
-  int ans = tree->rootGetResult(K);
+  // 3. DFS through the tree to collect points from bottom to up.
+  int answer = 0;
+  answer = dfs(bundle_size);
 
-  cout << ans << "\n";
+  cout << answer << "\n";
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
