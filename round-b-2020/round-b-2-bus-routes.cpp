f4f5c3090fad365f5c7f45515bd3a2fb92001cea
maurice <mauricehuang9@gmail.com>
2021-01-20 15:27:51 +0800

    Add note and refactor round B problem 2
    Include std header, refactor code, and add note.

--- round-b-2020/round-b-2-bus-routes.cpp
+++ round-b-2020/round-b-2-bus-routes.cpp
@@ -1,36 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
+void solve() {
 
-  long long N, D;
-  cin >> N >> D;
-  vector<long long> intervals(N);
-  for(int i = 0; i < N; ++i){
+  // 1. Get Input
+  int number_checkpoint, num_day;
+  cin >> number_checkpoint >> num_day;
+  vector<long long> intervals(number_checkpoint);
+  for (int i = 0; i < number_checkpoint; ++i) {
     long long in;
     cin >> in;
     intervals[i] = in;
   }
 
-  long long ans = D;
-  for(int i = N - 1; i >= 0 ; --i){
-    ans -= ans % intervals[i];
+  // 2. Iterate from the last bus, try catch up the previous bus as
+  //      late as possible, reduce the days from the latest_day_depart
+  long long latest_day_depart = num_day;
+  for (int i = number_checkpoint - 1; i >= 0 ; --i) {
+    long long days_to_nearest_previous_bus = latest_day_depart % intervals[i];
+    latest_day_depart -= days_to_nearest_previous_bus;
   }
 
-  cout << ans << endl;
+  cout << latest_day_depart << endl;
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
+
