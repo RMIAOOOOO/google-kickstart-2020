ff10dfc7691d4cb46616e7b551e828483859dfd9
maurice <mauricehuang9@gmail.com>
2021-01-25 02:28:09 +0800

    Add note and refactor round F problem 1
    Use std header, refactor code, and add note.

--- round-f-2020/round-f-1-atm-queue.cpp
+++ round-f-2020/round-f-1-atm-queue.cpp
@@ -1,38 +1,44 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-  int numberOfPeople, maximumWithdraw = 0;
-  vector<pair<int, int> > que;
-  cin >> numberOfPeople >> maximumWithdraw;
-  que.resize(numberOfPeople);
-  for(int i = 0; i < numberOfPeople; ++i){
+void solve() {
+
+  // 1. Get input
+  int num_people, max_withdraw = 0;
+  vector<pair<int, int> > amount_ith_person_pair;
+  cin >> num_people >> max_withdraw;
+
+  // 2. Create a pair vector for sorting by number of round take then by person number
+  amount_ith_person_pair.resize(num_people);
+  for (int i = 0; i < num_people; ++i) {
     int input;
     cin >> input;
-    int round = (input-1) / maximumWithdraw;
-    que[i] = make_pair(round, i);
+    int round = (input - 1) / max_withdraw;
+    amount_ith_person_pair[i] = make_pair(round, i);
   }
-  sort(que.begin(), que.end());
-  for(int i  = 0; i < numberOfPeople; ++i){
-    cout << que[i].second + 1;
-    if(i != numberOfPeople - 1) cout << " " ;
+
+  // 3. Sort the vector using pair's sort: sort first by round taken then by person number
+  sort(amount_ith_person_pair.begin(), amount_ith_person_pair.end());
+
+  // 4. Output the person number after sorting
+  for (int i  = 0; i < num_people; ++i) {
+    cout << amount_ith_person_pair[i].second + 1;
+    if (i != num_people - 1) cout << " " ;
   }
   cout << "\n";
 
 //  cout << ans << "\n";
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
