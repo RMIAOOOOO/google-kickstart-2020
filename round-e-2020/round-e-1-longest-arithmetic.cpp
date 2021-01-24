1d9334ab2355e44a4d33ec82ba81f37ec847bb51
maurice <mauricehuang9@gmail.com>
2021-01-24 15:38:41 +0800

    Add note and refactor round E problem 1
    Use std header, refactor code, and add note.

--- round-e-2020/round-e-1-longest-arithmetic.cpp
+++ round-e-2020/round-e-1-longest-arithmetic.cpp
@@ -1,34 +1,45 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include <sstream>
-#include <queue>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void solve() {
+
+  // 1. Get input
+  int N;
+  cin >> N;
+  vector<int> numbers(N);
+  for (int i = 0; i < N ; ++i) {
+    int in;
+    cin >> in;
+    numbers[i] = in;
+  }
+
+  // 2. Iterate through numbers to count the occurrence of same delta and update answer
+  int longest_length = 1;
+  int last_difference = numbers[1] - numbers[0];
+  int continuous_count = 1;
+  for (int i = 2 ; i < N ; ++i) {
+    int new_difference = numbers[i] - numbers[i-1];
+    if (new_difference == last_difference) {
+      continuous_count ++;
+      longest_length = max(continuous_count, longest_length);
+    } else {
+      last_difference = new_difference;
+      continuous_count = 1;
+    }
+  }
+
+  cout << longest_length + 1 << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    vector<int> prices;
-    int number, budget;
-    cin >> number >> budget;
-    for(int j = 0 ; j < number; ++j){
-        int price = 0;
-      cin >> price;
-      prices.push_back(price);
-    }
-    sort(prices.begin(), prices.end());
-    int ans = 0;
-    for(int z = 0; z < number; ++z){
-      if(budget >= prices[z]){
-        budget -= prices[z];
-        ans ++;
-      }else{
-        break;
-      }
-    }
-    cout << "Case #" << i+1 << ": " << ans << "\n";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
+    solve();
   }
 }
