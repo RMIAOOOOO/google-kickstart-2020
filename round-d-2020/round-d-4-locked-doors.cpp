1eb9148773e733000d8291aebdd652f6ecb8ea28
maurice <mauricehuang9@gmail.com>
2021-01-24 12:41:28 +0800

    Add note and refactor round D problem 4
    Use std header, refactor code from OOP style to procedual style, and
    add note.

--- round-d-2020/round-d-4-locked-doors.cpp
+++ round-d-2020/round-d-4-locked-doors.cpp
@@ -1,228 +1,188 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<stack>
-#include<iostream>
-#include<math.h>
-#include<memory>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-public:
-  Node* leftNode;
-  Node* rightNode;
-  Node* parentNode;
-  int subTreeCount;
-  int index;
-  int value;
-
-  Node(){
-    leftNode = nullptr;
-    rightNode = nullptr;
-    parentNode = nullptr;
-    subTreeCount = 0;
-    index = 0;
-    value = 0;
+// 4. DFS through doors to count how many sub-nodes a node has
+int dfsCountSubnode(vector<int> &subnode_count, const vector<int> &left_child, const vector<int> &right_child, int current_node){
+  int left_subnode_count = 0;
+  int right_subnode_count = 0;
+  if(left_child[current_node] != -1){
+    left_subnode_count = dfsCountSubnode(subnode_count, left_child, right_child, left_child[current_node]);
   }
-
-  int DFSAddWeight(){
-    int weight = 1;
-    if(leftNode != nullptr){
-      weight += leftNode->DFSAddWeight();
-    }
-    if(rightNode != nullptr){
-      weight += rightNode->DFSAddWeight();
-    }
-    subTreeCount = weight;
-    return weight;
+  if(right_child[current_node] != -1){
+    right_subnode_count = dfsCountSubnode(subnode_count, left_child, right_child, right_child[current_node]);
   }
-};
+  subnode_count[current_node] = 1 + left_subnode_count + right_subnode_count;
+  return subnode_count[current_node];
+}
 
 void solve(){
+
   // 1. Get input
   int N, Q;
   cin >> N >> Q;
   vector<int> difficulties(N-1);
-  vector<pair<int, int> > queires(Q);
-  for(int i = 0; i < N - 1; ++i ){
+  vector<int> query_start(Q);
+  vector<int> query_nth_room(Q);
+  for (int i = 0; i < N - 1; ++i) {
     int in;
     cin >> in;
     difficulties[i] = in;
   }
-  for(int i = 0 ; i < Q; ++i){
+  for (int i = 0 ; i < Q; ++i) {
     int start, step;
     cin >> start >> step;
-    queires[i] = make_pair(start, step);
+    query_start[i] = start;
+    query_nth_room[i] = step;
   }
 
 
-  // 2. Left, right, and i
-  vector<int> leftNext(N - 1);
-  stack<int> leftMaxStack;
+  // 2. Calculate the left and right closest door with higher but smallest difficulty
+  vector<int> left_next(N - 1);
+  stack<int> left_max_stack;
   for(int i = 0; i < N - 1 ; ++i){
-    while(leftMaxStack.size() > 0){
-      if(difficulties[i] > difficulties[leftMaxStack.top()]){
-        leftMaxStack.pop();
-      }else{
-        break;
-      }
-    }
-    if(leftMaxStack.size() == 0){
-      leftNext[i] = -1;
-    }else{
-      leftNext[i] = leftMaxStack.top();
-    }
-    leftMaxStack.push(i);
+    // 2.1  Remove the values smaller than the current value, the set it as the next difficult door.
+    while(left_max_stack.size() > 0 && difficulties[i] > difficulties[left_max_stack.top()])
+      left_max_stack.pop();
+    left_next[i] = (left_max_stack.size() == 0) ? -1 : left_max_stack.top();
+    // 2.2  Push the door to the stack for the following door to try difficulties.
+    left_max_stack.push(i);
   }
 
-  vector<int> rightNext(N - 1);
-  stack<int> rightMaxStack;
-  for(int i = N-2; i >= 0; --i){
-    while(rightMaxStack.size() > 0){
-      if(difficulties[i] > difficulties[rightMaxStack.top()]){
-        rightMaxStack.pop();
-      }else{
-        break;
-      }
-    }
-    if(rightMaxStack.size() == 0){
-      rightNext[i] = -1;
-    }else{
-      rightNext[i] = rightMaxStack.top();
-    }
-    rightMaxStack.push(i);
+  vector<int> right_next(N - 1);
+  stack<int> right_max_stack;
+  for (int i = N - 2; i >= 0; --i) {
+    while(right_max_stack.size() > 0 && difficulties[i] > difficulties[right_max_stack.top()])
+      right_max_stack.pop();
+    right_next[i] = (right_max_stack.size() == 0) ? -1 : right_max_stack.top();
+    right_max_stack.push(i);
   }
 
-  vector<int> next(N - 1);
+  // 3. Construct the Cartesian Tree based on the door opening next.
+  //    When a door is opened, the next door that would be more difficult to open but will
+  //      be opened first will be its parent node.
+  //    If a parent node is opened, all its children node are either going to be opened immediately
+  //      or are already opened.
+  vector<int> parent(N - 1);
+  vector<int> left_child(N - 1, -1);
+  vector<int> right_child(N - 1, -1);
+  int root_index = -1;
   for(int i = 0 ; i < N - 1; ++i){
-    if(rightNext[i] == -1 && leftNext[i] == -1){
-      next[i] = -1;
-    }else if(rightNext[i] == -1){
-      next[i] = leftNext[i];
-    }else if(leftNext[i] == -1){
-      next[i] = rightNext[i];
-    }else{
-      next[i] = (difficulties[leftNext[i]] > difficulties[rightNext[i]]) ? rightNext[i] : leftNext[i];
+    if (right_next[i] == -1 && left_next[i] == -1) {
+  //    3.1 Root node
+      parent[i] = -1;
+      root_index = i;
+    } else if (left_next[i] == -1 || ( right_next[i] != -1 && difficulties[left_next[i]] > difficulties[right_next[i]])) {
+  //    3.2 Select right door if there is no left door with higher difficulty or the right difficult door is easier than left
+      int right_child_index = right_next[i];
+      parent[i] = right_child_index;
+      right_child[right_child_index] = i;
+    } else {
+  //    3.3 Else select left door
+      int left_next_index = left_next[i];
+      parent[i] = left_next_index;
+      left_child[left_next_index] = i;
     }
   }
 
-  // 3. Tree construction
-  Node* nodes = new Node[N-1]();
-  int rootNodeIndex = -1;
-  for(int i = 0; i < N - 1; ++i){
-    nodes[i].index = i;
-    nodes[i].value = difficulties[i];
-    if(next[i] != -1){
-      nodes[i].parentNode = &nodes[next[i]];
-      if(i > next[i]){
-        nodes[next[i]].rightNode = &nodes[i];
-      }else{
-        nodes[next[i]].leftNode = &nodes[i];
-      }
-    }else{
-      rootNodeIndex = i;
-    }
-  }
+// 4. DFS sub-node count calculation.
+//    DFS through the tree to calculate how many sub-nodes every node has, which
+//      also represents how many doors need to be opened before opening door i.
+  vector<int> subnode_count(N, 0);
+  dfsCountSubnode(subnode_count, left_child, right_child, root_index);
 
-  // 4. Tree weight construction
-  nodes[rootNodeIndex].DFSAddWeight();
-
-  // 5. Binary step tree search construction
-  vector<vector<int> > parentLookup(N-1);
-  int lookupLength = 1; // N-1 = 5, lookup length = 2
-  while(pow(2, lookupLength) <= (N - 1) - 1){
-    lookupLength ++;
+// 5. Binary step tree search construction.
+//    Ex. lookup[2][0] = node 3's parent, = 1 level up
+//        lookup[2][1] = node 3's parent's[0] lookup, = 2 level up
+//        lookup[2][2] = node 3's [2] lookup's [2] lookup, = 4 level up
+//        lookup[2][5] = node 3's [4] lookup's [4] lookup, = 32 level up
+  int lookup_length = 1; // N-1 = 5, lookup length = 2
+  while(pow(2, lookup_length) <= (N - 1) - 1){
+    lookup_length ++;
   }
-  for(int i = 0; i < N - 1; ++i){
-    parentLookup[i].resize(lookupLength, -1);
-  }
-  for(int i = 0; i < lookupLength; ++i){
-    for(int j = 0; j < N - 1; ++j){
-      if(i == 0){
-        if(nodes[j].parentNode != nullptr){
-          parentLookup[j][i] = nodes[j].parentNode->index;
+  vector<vector<int> > binary_parent_lookup(N-1, vector<int>(lookup_length, -1));
+  for(int binary_step = 0; binary_step < lookup_length; ++binary_step){
+    for(int current_node_index = 0; current_node_index < N - 1; ++current_node_index){
+    // 5.1 All [0] level lookup is parent
+      if(binary_step == 0){
+        if(parent[current_node_index] != -1){
+          binary_parent_lookup[current_node_index][binary_step] = parent[current_node_index];
         }
       }else{
-        int stepsUpwards = i - 1;
-        if(parentLookup[j][stepsUpwards] != -1){
-          int parIndex = parentLookup[j][stepsUpwards];
-          parentLookup[j][i] = parentLookup[parIndex][stepsUpwards];
+    // 5.2 All [k] level lookup finds its' [k-1] level lookup's [k-1] level up
+    //     As 2^k level up is 2^(k-1) level up then another 2^(k-1) level up
+    //     As 8 level up is 4 level up then another 4 level up
+        if (binary_parent_lookup[current_node_index][binary_step - 1] != -1) {
+          int parent_step_upward = binary_step - 1;
+          int stepped_parent_index = binary_parent_lookup[current_node_index][parent_step_upward];
+          binary_parent_lookup[current_node_index][binary_step] = binary_parent_lookup[stepped_parent_index][parent_step_upward];
         }
       }
     }
   }
 
-//  for(int i = 0 ; i < N - 1; ++i){
-//    for(int j = 0; j < lookupLength; ++j){
-//      cout << parentLookup[i][j] << " ";
-//    }
-//    cout << endl;
-//  }
-//  cout << endl;
-
-  // 6. input case
-  for(int i = 0; i < Q; ++i){
-    int room = queires[i].first;
-    int step = queires[i].second;
-    if(step == 1){
-      cout << room << " ";
-      continue;
+  // 6. Process input cases
+  for (int i = 0; i < Q; ++i) {
+    int starting_room = query_start[i];
+    int door_open_quota = query_nth_room[i] - 1;
+    int first_door;
+    // 6.1 Select the first opened door (node)
+    if(starting_room == 1){
+      first_door = 0;
+    } else if( starting_room == N) {
+      first_door = N-2;
+    } else {
+      first_door = difficulties[starting_room - 2] > difficulties[starting_room-1] ? starting_room - 1 : starting_room-2;
+    }
+    // 6.2.1  Case 1: If the first opened door has sub-node count greater than steps.
+    //               Go all the way in same direction as the other direction won't be opened until
+    //               all other side is opened and steps used up.
+    if (subnode_count[first_door] >= door_open_quota) {
+      bool is_first_step_right = first_door == starting_room - 1 ;
+      if (is_first_step_right) {
+        cout << starting_room + door_open_quota << " ";
+      } else {
+        cout << starting_room - door_open_quota << " ";
+      }
     }else{
-      int firstDoor;
-      if(room == 1){
-        firstDoor = 0;
-      }else if( room == N){
-        firstDoor = N-2;
-      }else{
-        firstDoor = difficulties[room-2] > difficulties[room-1] ? room-1 : room-2;
+    // 6.2.2  Case 2: The firs opened door's sub-node count is not greater than total door to open.
+    //               Travel upward the tree until find a door that needs more doors opened than step
+      int max_completed_door = first_door;
+      // 6.2.2.1  Binary step up to find the parent node with the greatest sub-node count but lesser than the door open quota
+      for (int k = lookup_length - 1; k >= 0; --k){
+          // find the room such that the current node can be finished but need to go up for next step
+          // quota = 7, next node sub-node count = 4 or 5 or 6 go up
+          // quota = 7, next node sub-node count = 7, 8 stay
+        int parent_2_to_k_step_above = binary_parent_lookup[max_completed_door][k];
+        if (parent_2_to_k_step_above != -1 && subnode_count[parent_2_to_k_step_above] < door_open_quota) {
+          max_completed_door = parent_2_to_k_step_above;
+        }
       }
-      if(nodes[firstDoor].subTreeCount + 1 >= step ){
-        bool firstStepRight = firstDoor == room - 1 ;
-        if(firstStepRight){
-            cout << room + step -1 << " ";
-        }else{
-            cout << room - step + 1 << " ";
-        }
+      // 6.2.2.2  Use up the remaining door opening quota to open door in the direction of the next difficult door
+      int current_node_sub_door = subnode_count[max_completed_door];
+      int residual_steps = door_open_quota - current_node_sub_door;
+      bool is_next_difficult_door_at_left = parent[max_completed_door] < first_door;
+      if (is_next_difficult_door_at_left) {
+        int before_enter_room_number = parent[max_completed_door] + 2;
+          cout << before_enter_room_number - residual_steps << " ";
       }else{
-        int upwardFirstDoor = firstDoor;
-//        cout << "(upwardFirstDoor original) " << upwardFirstDoor << ". " << endl;
-        for(int k = lookupLength - 1; k >= 0; --k){
-            // find the room such that the current node can be finished but need to go up for next step
-            // room = 8, next sub = 4.5.6 go up
-            // room = 8, next sub = 7.8.9 stay
-          if(parentLookup[upwardFirstDoor][k] != -1 && nodes[parentLookup[upwardFirstDoor][k]].subTreeCount < step - 1){
-            upwardFirstDoor = parentLookup[upwardFirstDoor][k];
-          }
-        }
-//        cout << "(upwardFirstDoorIndex) " << upwardFirstDoor << ". " << endl;
-
-        int currentNodeSubDoor = nodes[upwardFirstDoor].subTreeCount;
-        int residualSteps = step - currentNodeSubDoor - 1;
-        bool rightToLeft = nodes[upwardFirstDoor].parentNode->index < firstDoor;
-        if(rightToLeft ){
-          // before: room 3 residual step = 2
-          int beforeEnterRoomNumber = nodes[upwardFirstDoor].parentNode->index+2;
-            cout << beforeEnterRoomNumber - residualSteps << " ";
-        }else{
-          int beforeEnterRoomNumber = nodes[upwardFirstDoor].parentNode->index+1;
-          int remainingRoom = room - beforeEnterRoomNumber;
-            cout << beforeEnterRoomNumber + residualSteps << " ";
-        }
-
+        int before_enter_room_number = parent[max_completed_door] + 1;
+          cout << before_enter_room_number + residual_steps << " ";
       }
+
     }
   }
   cout << "\n";
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
