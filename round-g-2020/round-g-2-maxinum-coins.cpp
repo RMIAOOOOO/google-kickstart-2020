4e8609f02892c7193215565093ba30ae86e2c259
maurice <mauricehuang9@gmail.com>
2021-01-25 17:29:19 +0800

    Add note and refactor round G problem 2
    Use std header, refactor code, and add note.

--- round-g-2020/round-g-2-maxinum-coins.cpp
+++ round-g-2020/round-g-2-maxinum-coins.cpp
@@ -1,66 +1,66 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-  int squareLength;
-  cin >> squareLength;
-  vector< vector<int> > square;
-  square.resize(squareLength);
-  for(int i = 0 ; i < squareLength; ++i){
-    square[i].resize(squareLength);
-    for(int j = 0 ; j < squareLength; ++j){
+void solve() {
+
+  // 1. Get input
+  int square_width;
+  cin >> square_width;
+  vector<vector<int> > matrix(square_width, vector<int>(square_width, 0));
+  for(int i = 0 ; i < square_width; ++i){
+    for(int j = 0 ; j < square_width; ++j){
       int input;
       cin >> input;
-      square[i][j] = input;
+      matrix[i][j] = input;
     }
   }
+
   long int ans = 0;
-  for(int i = 0 ; i < squareLength - 1 ; ++i ){
-    int numberOfElement = i+1;
-    long int sum = 0;
-    for(int j = 0 ; j < numberOfElement; ++j){
-      int row = squareLength - i + j -1;
+  // 2. Iterate through bottom left diagonals
+  for (int i = 0 ; i < square_width - 1 ; ++i) {
+    int num_element_in_diagonal = i + 1;
+    long int sum_bottom_left_diagonal = 0;
+    for(int j = 0 ; j < num_element_in_diagonal; ++j){
+      int row = square_width - i + j -1;
       int col = j;
-      sum += square[row][col];
+      sum_bottom_left_diagonal += matrix[row][col];
     }
-    ans = max(sum, ans);
+    ans = max(sum_bottom_left_diagonal, ans);
   }
 
-
-  long int sum2 = 0;
-  for(int j = 0 ; j < squareLength; ++j){
+  // 3. Try middle diagonals
+  long int sum_logest_diagonal = 0;
+  for (int j = 0 ; j < square_width; ++j) {
     int row = j;
     int col = j;
-    sum2 += square[row][col];
+    sum_logest_diagonal += matrix[row][col];
   }
-  ans = max(sum2, ans);
+  ans = max(sum_logest_diagonal, ans);
 
-  for(int i = 0 ; i < squareLength - 1 ; ++i ){
-    int numberOfElement = i+1;
-    long int sum = 0;
-    for(int j = 0 ; j < numberOfElement; ++j){
+  // 4. Iterate through upper right diagonals
+  for (int i = 0 ; i < square_width - 1 ; ++i) {
+    int num_element_in_diagonal = i+1;
+    long int sum_upper_right_diagonal = 0;
+    for(int j = 0 ; j < num_element_in_diagonal; ++j){
       int row = j;
-      int col = squareLength + j - i -1;
-      sum += square[row][col];
+      int col = square_width + j - i -1;
+      sum_upper_right_diagonal += matrix[row][col];
     }
-    ans = max(sum, ans);
+    ans = max(sum_upper_right_diagonal, ans);
   }
-
 
   cout << ans << "\n";
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
