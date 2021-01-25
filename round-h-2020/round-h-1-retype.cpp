1d23db768345cf22aa0bfc0f057d96f7b0068ac7
maurice <mauricehuang9@gmail.com>
2021-01-26 04:04:38 +0800

    Add note and refactor round H problem 1
    Use std header, refactor code, and add note.

--- round-h-2020/round-h-1-retype.cpp
+++ round-h-2020/round-h-1-retype.cpp
@@ -1,26 +1,29 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-  long long N, K, S;
-  cin >> N >> K >> S;
-  long long ans = K - 1 + 1 + N;
-  ans = min(ans, K - 1 + K - S + N - S + 1);
+void solve() {
+
+  // 1. Get input
+  long long total_level, current_level, weapon_level;
+  cin >> total_level >> current_level >> weapon_level;
+
+  // 2. Calculate two potion's cost and compare
+  long long restart_cost = (current_level - 1) + 1 + total_level;
+  long long go_back_cost = (current_level - 1) + (current_level - weapon_level) + (total_level - weapon_level + 1);
+  long long ans = min(restart_cost, go_back_cost);
+
   cout << ans << "\n";
-//  cout << ans << "\n";
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
