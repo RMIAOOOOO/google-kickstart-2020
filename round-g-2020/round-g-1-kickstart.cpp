551d990635b7d43b9322b6dd403fc7d44c464905
maurice <mauricehuang9@gmail.com>
2021-01-25 16:55:07 +0800

    Add note and refactor round G problem 1
    Use std header, refactor code, and add note.

--- round-g-2020/round-g-1-kickstart.cpp
+++ round-g-2020/round-g-1-kickstart.cpp
@@ -1,44 +1,49 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
+void solve() {
+
+  // 1. Get input
   string S;
   cin >> S;
-  vector<char> notedown;
-  notedown.resize(S.length(), ' ');
-  if(S.length() <= 6){
-    cout << "0\n";
-  }else {
-    for(int i = 0 ; i < S.length() - 3; ++i){
-      if(S[i] == 'K' && S[i+1] == 'I' && S[i+2] == 'C' && S[i+3] == 'K') notedown[i] = 'K';
+
+  // 2. Find the word within the text
+  vector<char> word_found(S.length(), ' ');
+  for (int i = 0 ; i + 3 < S.length(); ++i) {
+    if (S[i] == 'K' && S[i+1] == 'I' && S[i+2] == 'C' && S[i+3] == 'K') {
+      word_found[i] = 'K';
     }
-    for(int i = 0 ; i < S.length() - 4; ++i){
-      if(S[i] == 'S' && S[i+1] == 'T' && S[i+2] == 'A' && S[i+3] == 'R'&& S[i+4] == 'T') notedown[i] = 'S';
-    }
-    int ans = 0;
-    int Kcount = 0;
-    for(int i = 0; i < S.length()-3; ++i){
-      if(notedown[i] == 'K'){
-        Kcount ++;
-      }else if(notedown[i] == 'S'){
-        ans += Kcount;
-      }
-    }
-    cout << ans << "\n";
   }
+  for (int i = 0 ; i + 4 < S.length(); ++i) {
+    if (S[i] == 'S' && S[i+1] == 'T' && S[i+2] == 'A' && S[i+3] == 'R'&& S[i+4] == 'T'){
+      word_found[i] = 'S';
+    }
+  }
+
+  // 3. Iterate through the words, when find 'K', add it for matching for upcoming 'S',
+  //      when find 'S', match up with previous 'K' and update answer.
+  int ans = 0;
+  int k_count = 0;
+  for (int i = 0; i < word_found.size() ; ++i) {
+    if (word_found[i] == 'K'){
+      k_count ++;
+    } else if(word_found[i] == 'S') {
+      ans += k_count;
+    }
+  }
+
+  cout << ans << "\n";
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
