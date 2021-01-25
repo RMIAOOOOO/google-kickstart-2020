5a03354fc80c867b5291175bb844e6530bf4e5e0
maurice <mauricehuang9@gmail.com>
2021-01-26 06:54:07 +0800

    Add note and refactor round H problem 3
    Use std header, refactor code, and add note.

--- round-h-2020/round-h-3-rugby.cpp
+++ round-h-2020/round-h-3-rugby.cpp
@@ -1,65 +1,56 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<sstream>
-#include<queue>
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
+void solve() {
 
   // 1. Get Input
   int N;
   cin >> N;
-  vector<long> XCoord(N);
-  vector<long> YCoord(N);
-  for(int i = 0; i < N; ++i){
-    int inX, inY;
-    cin >> inX >> inY;
-    long long longX = inX;
-    long long longY = inY;
-    XCoord[i] = longX;
-    YCoord[i] = longY;
+  vector<long long> x_coord(N);
+  vector<long long> y_coord(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> x_coord[i] >> y_coord[i];
   }
 
-  // 2. get row
-  long long sumY = 0;
+  int mid_index = N / 2;
 
-  int midIndex = N / 2;
-  sort(YCoord.begin(), YCoord.end());
-  long long finalY = YCoord[midIndex];
-  for(int i = 0; i < N; ++i){
-    long long individualCost = abs(YCoord[i] - finalY);
-    sumY += individualCost;
+  // 2. Get vertical movement cost
+  //    Align player along the player in horizontal middle
+  sort(y_coord.begin(), y_coord.end());
+  long long final_y = y_coord[mid_index];
+  long long cost_vertical = 0;
+  for (int i = 0; i < N; ++i) {
+    cost_vertical += abs(y_coord[i] - final_y);
   }
 
-  // 3. get col
-  long long sumX = 0;
-
-  sort(XCoord.begin(), XCoord.end());
-  for(int i = 0; i < N; ++i){
-    XCoord[i] -= i;
+  // 3. Get horizontal movement cost, then sort player by X, then adjust best step based on index, sort again
+  //    Align player along the player in the adjusted horizontal middle
+  sort (x_coord.begin(), x_coord.end());
+  for (int i = 0; i < N; ++i) {
+    x_coord[i] -= i;
   }
-  sort(XCoord.begin(), XCoord.end());
-  long long finalX = XCoord[midIndex];
-  for(int i = 0; i < N; ++i){
-    long long individualCost = abs(XCoord[i] - (finalX));
-    sumX += individualCost;
+  sort(x_coord.begin(), x_coord.end());
+
+  long long final_x = x_coord[mid_index];
+  long long cost_horizontal = 0;
+  for (int i = 0; i < N; ++i) {
+    cost_horizontal += abs(x_coord[i] - (final_x));
   }
 
-  // 4. calculate cost
-  long long ans = sumX + sumY;
+  // 4. Calculate total cost
+  long long ans = cost_horizontal + cost_vertical;
   cout << ans << "\n";
-
-
 }
 
-int main(){
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
   int t;
   cin >> t;
-  for(int i = 0; i < t; ++i){
-    cout << "Case #" << i+1 << ": ";
+
+  for (int i = 0; i < t; ++i) {
+    cout << "Case #" << i+1 << ": " ;
     solve();
   }
 }
